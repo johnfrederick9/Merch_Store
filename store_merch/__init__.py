ed0b9621851bf7538723a0e77731d164202067e1
John Frederick Gelay <gelayjohnfrederick0@gmail.com>
2025-07-26 18:04:49 +0800

    feat(code):global-changes

---
+++ store_merch/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'store_merch.apps.StoreMerchConfig'  # replace with your app name
--- store_merch/apps.py
+++ store_merch/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
-class StoreMerchConfig(AppConfig):
+class StoreMerchConfig(AppConfig):  # Use your actual app name
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store_merch'
+
+    def ready(self):
+        import store_merch.signals
